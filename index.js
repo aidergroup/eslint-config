2f8c763187ae30bb0400081890e4ded79b070b9b
David Öhlin <hello@davidohl.in>
2021-06-07 16:46:32 +0200

    disable jsx-a11y/label-has-associated-control

--- index.js
+++ index.js
@@ -35,5 +35,6 @@ module.exports = {
     'no-alert': 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/label-has-associated-control': 0,
   },
 }
